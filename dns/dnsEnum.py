6bc47bfb036b607b037f8637fbaa0f6b40315a0e
rbielawa14 <rh52513n@pace.edu>
2026-04-17 10:31:31 -0400

    Edits to dns functionality

--- dns/dnsEnum.py
+++ dns/dnsEnum.py
@@ -3,11 +3,13 @@ import dns.resolver
 def dnsEnum(domain):
     try:
         print(f"Getting DNS Information for {domain}")
-        for a in dns.resolver.resolve(domain, 'A'):
-            if a: print(f"Results: {a.to_text()}")
-            else: continue
+        for a in dns.resolver.resolve(domain, 'A'): 
+            print("A Record: ", a.to_text())
+        for ns in dns.resolver.resole(domain, 'NS'):
+            print("NS Record: ", ns.to_text())
+        for MX in dns.resolver.resole(domain, 'MX'):
+            print("NS Record: ", ns.to_text())
     except Exception as e:
         print(f"Unable to Resolve DNS Information: {e}")
 
-
-dnsEnum("cityinthe.cloud")
+dnsEnum("www.google.com")
